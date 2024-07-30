7abde8ab6316915803ae60755258fdeaeb572f2f
Jabar141 <97104195+Jabar141@users.noreply.github.com>
2024-07-30 10:23:01 +0530

    Update app.mjs

--- service/src/app.mjs
+++ service/src/app.mjs
@@ -1,1 +1,15 @@
+const {reviewProductChanges} = require('./filterProductChanges');
+const express = require('express');
+const app = express();
+const port = 8080;
 
+app.get('/',  (req, res) => {
+  res.send('Hello World!');
+  });
+  
+
+  app.post('/products/:id', async (req, res) => {
+    var changes=await reviewProductChanges(req.params.id);
+    res.send(changes);
+    });
+    
