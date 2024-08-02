fb19b840bd8c10d453f4d5d406a93d183c204f3d
Jabar141 <97104195+Jabar141@users.noreply.github.com>
2024-08-02 16:40:14 +0530

    Update introspect.js

--- service/src/introspect.js
+++ service/src/introspect.js
@@ -1,1 +1,18 @@
+const axios = require('axios');
+let data = '';
 
+
+module.exports.introspectToken = async(token) =>{
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://auth.us-central1.gcp.commercetools.com/oauth/introspect?token='+token,
+        headers: { 
+          'Content-Type': 'application/json', 
+          'Authorization': 'Basic eFBOd0Z6M1Fob29RMHVScXdmaFVuUHUwOkh0NVZYNmd3aUVyS1FrMU5JaW43cFR0YldSY05FeXh0'
+        },
+        data : data
+      };
+var tokenResponse=await axios.request(config);
+return tokenResponse.data;
+}
