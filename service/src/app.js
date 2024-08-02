257828956a3ff82f177ac66c195c3077c94d269b
Jabar141 <97104195+Jabar141@users.noreply.github.com>
2024-08-02 16:59:13 +0530

    Update app.js

--- service/src/app.js
+++ service/src/app.js
@@ -1,22 +1,37 @@
 
 const express = require('express');
 const { reviewProductChanges } = require('./filterProductChanges');
+const { introspectToken } = require('./introspect');
 
 
 const app = express();
-const port = 8080;
+const port = 8081;
 
-app.get('/',  (req, res) => {
+app.use(express.json())
+app.get('/', (req, res) => {
   res.send('Hello World!');
-  });
-  
+});
 
-  app.post('/products/:id', async (req, res) => {
-    var changes=await reviewProductChanges(req.params.id);
+
+app.post('/products/:id', async (req, res) => {
+  var changes = await reviewProductChanges(req.params.id);
+  res.send(changes);
+});
+
+app.post('/validateProducts', async (req, res) => {
+  console.log(req.body.resource.id);
+  var token = await introspectToken(req.headers.authorization);
+  if (token.active == false) {
+    res.status(401).send(token);
+  }
+  else {
+    var changes = await reviewProductChanges(req.body.resource.id);
     res.send(changes);
-    });
+  }
+});
 
-  
-      app.listen(port, () => {
-        console.log(`Server running at http://localhost:${port}`);
-      });
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
